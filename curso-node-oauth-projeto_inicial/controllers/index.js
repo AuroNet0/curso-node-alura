7e14d9c205469252144871340ed8a06f473d81a8
AuroNet0 <auroneto04@gmail.com>
2026-02-19 13:18:42 -0300

    Aula 03

--- curso-node-oauth-projeto_inicial/controllers/index.js
+++ curso-node-oauth-projeto_inicial/controllers/index.js
@@ -35,6 +35,7 @@ exports.login = async (req, res, next) => {
   const user = await User.findOne(email, password);
   try {
     if (user) {
+      req.session.user = user;
       res.redirect("/members");
     } else {
       res.render("index");
@@ -46,12 +47,18 @@ exports.login = async (req, res, next) => {
 };
 
 exports.checkAuth = (req, res, next) => {
-    //Estrutura inicial para implementação de autenticação.
-    const auth = false;
-    if (auth) {
-        next();
-    } else {
-        res.redirect("/");
-    }
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+};
 
+exports.logout = (req, res, next) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect('/');
+    });
 };
